a80e782b28bb99a42b28b015d883d0b56e0453f8
Alopex6414 <1029535012@qq.com>
2025-05-17 18:04:06 +0800

    resize MainWindow size and button location

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel
 from PyQt6 import QtCore
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Nova UI")
-        self.setGeometry(300, 300, 250, 150)
+        self.setGeometry(QtCore.QRect(100, 100, 640, 480))
         # create a button
         self.button =QPushButton("Click", self)
-        self.button.setGeometry(QtCore.QRect(200, 200, 200, 200))
+        self.button.setGeometry(QtCore.QRect(200, 200, 50, 20))
+        # create a label
+        self.label = QLabel(self)
 
 
 if __name__ == '__main__':
