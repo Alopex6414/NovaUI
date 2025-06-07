11a7e92e2d5931433678cd7638a0f695bfcbf501
Alopex6414 <1029535012@qq.com>
2025-06-07 21:08:10 +0800

    create home page for ui

--- novaUI/mainwindow.cpp
+++ novaUI/mainwindow.cpp
@@ -6,6 +6,22 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    // initialize comboBox Test
+    ui->comboBoxTest->clear();
+    ui->comboBoxTest->addItem("IPMP Test");
+    ui->comboBoxTest->addItem("PMP Test");
+    ui->comboBoxTest->addItem("NPDP Test");
+    ui->comboBoxTest->addItem("Communication Test");
+    ui->comboBoxTest->addItem("Computer Technology & Software Test");
+    ui->comboBoxTest->setCurrentIndex(0);
+    // initialize comboBox Level
+    ui->comboBoxLevel->clear();
+    ui->comboBoxLevel->addItem("Common");
+    ui->comboBoxLevel->setCurrentIndex(0);
+    // initialize comboBox Level
+    ui->comboBoxSubject->clear();
+    ui->comboBoxSubject->addItem("Common");
+    ui->comboBoxSubject->setCurrentIndex(0);
 }
 
 MainWindow::~MainWindow()
